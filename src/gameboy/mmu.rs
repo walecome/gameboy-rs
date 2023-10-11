215ebaca672da8190d4c6449d7c5341a25b81eca
Alexander Wilkens <alexander.wilkens96@gmail.com>
2023-10-11 20:35:50 +0200

    Expand rendering a little bit

--- src/gameboy/mmu.rs
+++ src/gameboy/mmu.rs
@@ -52,7 +52,6 @@ pub struct IO {
     timer_and_divider: Vec<u8>,
     audio: Vec<u8>,
     wave_pattern: Vec<u8>,
-    lcd: Vec<u8>,
     vram_bank_select: u8,
     boot_rom_disabled: u8,
     vram_dma: Vec<u8>,
@@ -76,61 +75,12 @@ impl IO {
             timer_and_divider: byte_vec_for_range(0xFF00, 0xFF07),
             audio: byte_vec_for_range(0xFF10, 0xFF26),
             wave_pattern: byte_vec_for_range(0xFF30, 0xFF3F),
-            lcd: byte_vec_for_range(0xFF40, 0xFF4B),
             vram_bank_select: 0x00,
             boot_rom_disabled: 0x00,
             vram_dma: byte_vec_for_range(0xFF51, 0xFF55),
             bg_obj_palettes: byte_vec_for_range(0xFF68, 0xFF6B),
             wram_bank_select: 0x00,
         }
-    }
-
-    fn read(&self, address: Address) -> u8 {
-        let select_byte: u8 = match address.value() {
-            0xFF00..=0xFF70 => (address.value() & 0xFF) as u8,
-            _ => panic!("Trying to read IO outside mapped area: {:#06X}", address.value()),
-        };
-
-        match select_byte {
-            0x00 => self.joypad_input,
-            0x01 => self.serial_transfer.0,
-            0x02 => self.serial_transfer.1,
-            0x04..=0x07 => self.timer_and_divider[(select_byte - 0x04) as usize],
-            0x10..=0x26 => self.audio[(select_byte - 0x10) as usize],
-            0x30..=0x3F => self.wave_pattern[(select_byte - 0x30) as usize],
-            0x40..=0x4B => self.lcd[(select_byte - 0x40) as usize],
-            0x4F => self.vram_bank_select,
-            0x50 => self.boot_rom_disabled,
-            0x51..=0x55 => self.vram_dma[(select_byte - 0x51) as usize],
-            0x68..=0x6B => self.bg_obj_palettes[(select_byte - 0x68) as usize],
-            0x70 => self.wram_bank_select,
-            _ => panic!("Read for unmapped IO address: {:#06X}", address.value()),
-        }
-    }
-
-    fn write(& mut self, address: Address, value: u8) {
-        let select_byte: u8 = match address.value() {
-            0xFF00..=0xFF70 => (address.value() & 0xFF) as u8,
-            _ => panic!("Trying to write IO outside mapped area: {:#06X}", address.value()),
-        };
-
-        let target: &mut u8 = &mut match select_byte {
-            0x00 => self.joypad_input,
-            0x01 => self.serial_transfer.0,
-            0x02 => self.serial_transfer.1,
-            0x04..=0x07 => self.timer_and_divider[(select_byte - 0x04) as usize],
-            0x10..=0x26 => self.audio[(select_byte - 0x10) as usize],
-            0x30..=0x3F => self.wave_pattern[(select_byte - 0x30) as usize],
-            0x40..=0x4B => self.lcd[(select_byte - 0x40) as usize],
-            0x4F => self.vram_bank_select,
-            0x50 => self.boot_rom_disabled,
-            0x51..=0x55 => self.vram_dma[(select_byte - 0x51) as usize],
-            0x68..=0x6B => self.bg_obj_palettes[(select_byte - 0x68) as usize],
-            0x70 => self.wram_bank_select,
-            _ => panic!("Write for unmapped IO address: {:#06X}", address.value()),
-        };
-
-        *target = value;
     }
 }
 
@@ -176,7 +126,7 @@ impl MMU {
             0xE000..=0xFDFF => panic!("Read access for prohibited memory area"),
             0xFE00..=0xFE9F => todo!("Read OAM"),
             0xFEA0..=0xFEFF => panic!("Read access for prohibited memory area"),
-            0xFF00..=0xFF7F => self.io.read(address),
+            0xFF00..=0xFF7F => self.read_io(address),
             0xFF80..=0xFFFE => self.high_ram[address.index_value() - 0xFF80],
             0xFFFF => self.interrupt_enable,
         }
@@ -204,7 +154,7 @@ impl MMU {
             0xE000..=0xFDFF => panic!("Write access for prohibited memory area"),
             0xFE00..=0xFE9F => todo!("Write OAM"),
             0xFEA0..=0xFEFF => panic!("Write access for prohibited memory area"),
-            0xFF00..=0xFF7F => self.io.write(address, value),
+            0xFF00..=0xFF7F => self.write_io(address, value),
             0xFF80..=0xFFFE => self.high_ram[address.index_value() - 0xFF80] = value,
             0xFFFF => self.interrupt_enable = value,
         }
@@ -213,5 +163,52 @@ impl MMU {
     pub fn write_word(&mut self, address: Address, value: Word) {
         self.write(address, value.low());
         self.write(address.next(), value.high());
+    }
+
+
+    fn read_io(&self, address: Address) -> u8 {
+        let select_byte: u8 = match address.value() {
+            0xFF00..=0xFF70 => (address.value() & 0xFF) as u8,
+            _ => panic!("Trying to read IO outside mapped area: {:#06X}", address.value()),
+        };
+
+        match select_byte {
+            0x00 => self.io.joypad_input,
+            0x01 => self.io.serial_transfer.0,
+            0x02 => self.io.serial_transfer.1,
+            0x04..=0x07 => self.io.timer_and_divider[(select_byte - 0x04) as usize],
+            0x10..=0x26 => self.io.audio[(select_byte - 0x10) as usize],
+            0x30..=0x3F => self.io.wave_pattern[(select_byte - 0x30) as usize],
+            0x40..=0x4B => self.video.lcd().read(select_byte),
+            0x4F => self.io.vram_bank_select,
+            0x50 => self.io.boot_rom_disabled,
+            0x51..=0x55 => self.io.vram_dma[(select_byte - 0x51) as usize],
+            0x68..=0x6B => self.io.bg_obj_palettes[(select_byte - 0x68) as usize],
+            0x70 => self.io.wram_bank_select,
+            _ => panic!("Read for unmapped IO address: {:#06X}", address.value()),
+        }
+    }
+
+    fn write_io(& mut self, address: Address, value: u8) {
+        let select_byte: u8 = match address.value() {
+            0xFF00..=0xFF70 => (address.value() & 0xFF) as u8,
+            _ => panic!("Trying to write IO outside mapped area: {:#06X}", address.value()),
+        };
+
+        match select_byte {
+            0x00 => self.io.joypad_input = value,
+            0x01 => self.io.serial_transfer.0 = value,
+            0x02 => self.io.serial_transfer.1 = value,
+            0x04..=0x07 => self.io.timer_and_divider[(select_byte - 0x04) as usize] = value,
+            0x10..=0x26 => self.io.audio[(select_byte - 0x10) as usize] = value,
+            0x30..=0x3F => self.io.wave_pattern[(select_byte - 0x30) as usize] = value,
+            0x40..=0x4B => self.video.lcd_mut().write(select_byte, value),
+            0x4F => self.io.vram_bank_select = value,
+            0x50 => self.io.boot_rom_disabled = value,
+            0x51..=0x55 => self.io.vram_dma[(select_byte - 0x51) as usize] = value,
+            0x68..=0x6B => self.io.bg_obj_palettes[(select_byte - 0x68) as usize] = value,
+            0x70 => self.io.wram_bank_select = value,
+            _ => panic!("Write for unmapped IO address: {:#06X}", address.value()),
+        };
     }
 }
--- src/gameboy/video.rs
+++ src/gameboy/video.rs
@@ -1,14 +1,126 @@
 use super::address::Address;
 
+const DOTS_PER_LINE: usize = 456;
+const DOTS_PER_FRAME: usize = 70224;
+const DOTS_OAM_SCAN: usize = 80;
+const MIN_DOTS_DRAW_PIXELS: usize = 172;
+
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+pub struct Lcd {
+    scy: u8,
+    scx: u8,
+}
+
+impl Lcd {
+    fn new() -> Self {
+        Self { scy: 0, scx: 0 }
+    }
+
+    pub fn read(&self, _select_byte: u8) -> u8 {
+        todo!();
+    }
+
+    pub fn write(&mut self, _select_byte: u8, _value: u8) {
+        todo!();
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum VideoMode {
+    OamScan,
+    DrawPixels,
+    HorizontalBlank,
+    VerticalBlank,
+}
+
 pub struct Video {
     vram: Vec<u8>,
+    current_mode: VideoMode,
+    ly: usize,
+    current_dot: usize,
+    lcd: Lcd,
 }
 
 impl Video {
     pub fn new() -> Self {
         Self {
             vram: vec![0x00; 0x4000],
+            current_mode: VideoMode::OamScan,
+            ly: 0,
+            current_dot: 0,
+            lcd: Lcd::new(),
         }
+    }
+
+    pub fn tick(&mut self, elapsed_cycles: usize) {
+        self.current_dot += elapsed_cycles;
+
+        let next_mode = if let Some(mode) = self.get_mode_transition() {
+            mode
+        } else {
+            return;
+        };
+
+        match next_mode {
+            VideoMode::OamScan => {
+                assert_eq!(self.current_mode, VideoMode::VerticalBlank);
+                // TODO: Implement
+            },
+            VideoMode::DrawPixels => {
+                assert_eq!(self.current_mode, VideoMode::OamScan);
+                // TODO: Implement
+            },
+            VideoMode::HorizontalBlank => {
+                assert_eq!(self.current_mode, VideoMode::DrawPixels);
+                // TODO: Implement
+            },
+            VideoMode::VerticalBlank => {
+                assert_eq!(self.current_mode, VideoMode::HorizontalBlank);
+                // TODO: Implement
+            },
+        }
+
+        self.current_mode = next_mode;
+    }
+
+    fn get_mode_transition(&mut self) -> Option<VideoMode> {
+        let point = self.current_point();
+        Some(match self.current_mode {
+            VideoMode::OamScan => {
+                if point.x < DOTS_OAM_SCAN {
+                    return None;
+                }
+                VideoMode::OamScan
+            },
+
+            VideoMode::DrawPixels => {
+                // TODO: Calculate MODE 3 penalty
+                let elapsed_draw_pixels = point.x - DOTS_OAM_SCAN;
+                if elapsed_draw_pixels < MIN_DOTS_DRAW_PIXELS {
+                    return None
+                }
+                VideoMode::DrawPixels
+            },
+
+            VideoMode::HorizontalBlank => {
+                assert!(point.y <= 144);
+                if point.y != 144 {
+                    return None
+                }
+                VideoMode::VerticalBlank
+            },
+
+            VideoMode::VerticalBlank => {
+                if self.current_dot < DOTS_PER_FRAME {
+                    return None
+                }
+                VideoMode::OamScan
+            },
+        })
     }
 
     pub fn write_vram(&mut self, address: Address, value: u8) {
@@ -17,5 +129,20 @@ impl Video {
 
     pub fn read_vram(&self, address: Address) -> u8 {
         self.vram[address.index_value()]
+    }
+
+    pub fn lcd_mut(&mut self) -> &mut Lcd {
+        &mut self.lcd
+    }
+
+    pub fn lcd(&self) -> &Lcd {
+        &self.lcd
+    }
+
+    fn current_point(&self) -> Point {
+        Point {
+            x: self.current_dot / DOTS_PER_LINE,
+            y: self.current_dot % DOTS_PER_LINE,
+        }
     }
 }
